32c835d70cb91b1518158a11f68ce04648b66ceb
Ivo Vondrak <ivo.vondrak@gmail.com>
2024-03-22 17:40:31 +0100

    Petri Net 1.0

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from transition import Transition
 from petrinetgraph import PetriNetGraph, GPlace, GTransition
 
 
+
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
