f12f2229c65ae61c35b0c45052cb805b1b4f9d14
Ivo Vondrak <ivo.vondrak@gmail.com>
2024-03-22 18:35:42 +0100

    Petri Net 1.0.1

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ import time
 import random
 
 
-
-
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
